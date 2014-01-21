2b0c6ac9cce7dd49c7f7867890a66691916e3cf9
Your Name <blake.jennings@gmail.com>
2014-01-21 14:58:48 -0800

    whoops, added files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stalkmore::Application.config.secret_key_base = 'd22e6613c6312e2d8387de9197ea933f61caf08dc4c50b1c8c17318d3b0d5bdfbeed753588da13786d0f1bedc4e3c22273077e20ed240e48b307075994a859a8'
