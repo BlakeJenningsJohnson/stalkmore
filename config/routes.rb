5887749889408530c2b8df29b52a71eb56105b03
Melinda Mizuta <mkmizuta@gmail.com>
2014-01-22 11:35:29 -0800

    added bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Stalkmore::Application.routes.draw do
+
+  get "/test", to: "test#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
