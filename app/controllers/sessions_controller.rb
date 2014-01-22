add4823d681635aeb7f380a1027221231b363dde
Your Name <blake.jennings@gmail.com>
2014-01-22 15:48:34 -0800

    added notice

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_or_create_from_omniauth(auth_hash)
     session[:user_id] = @user.id
-    redirect_to "/"
+    redirect_to "/", notice: "Welcome! Get Stalking ;)"
   end
 
   def sign_out
