1c9e7dff5724e12dca9ac9f24a1ab880d4d26d99
Anne Kimsey <anne.kimsey@gmail.com>
2014-01-22 15:29:22 -0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Stalkmore::Application.routes.draw do
 
   root to: "sessions#show"
   get "users/sign_in", to: redirect("/auth/developer"), as: :sign_in
-  get "/auth/:provider/callback", to: "sessions#create"
   get "/users/sign_out", to: "sessions#sign_out", as: :sign_out
-  post "/auth/:provider/callback", to: "sessions#create"
+  # get "/auth/:provider/callback", to: "sessions#create"
+  # post "/auth/:provider/callback", to: "sessions#create" # This is what the below 'match' method does.
   match "/auth/:provider/callback", to: "sessions#create", via: [:get, :post]
   get "/test", to: "test#index"
   # The priority is based upon order of creation: first created -> highest priority.
