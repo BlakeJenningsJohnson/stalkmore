7b7ee30cad9b6b3a5fa45b23310219d3a3359ea9
Christina Thompson <c.thompsonms@gmail.com>
2014-01-23 11:39:00 -0800

    config twitter api

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer # unless Rails.env.production?
-  provider :twitter, ENV["TWITTER_CLIENT_ID"], ENV["TWITTER_CLIENT_SECRET"]
 end
