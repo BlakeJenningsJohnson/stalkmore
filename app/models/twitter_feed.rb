ba5632600532cdfb90b87e98ae38e3d91fcf4abd
Anne Kimsey <anne.kimsey@gmail.com>
2014-01-28 11:00:35 -0800

    Twitter posts can not be duplicated

--- app/models/twitter_feed.rb
+++ app/models/twitter_feed.rb
@@ -1,9 +1,5 @@
 class TwitterFeed < Feed
 
-  # def get_posts
-  #   interact with API
-  # end
-
   def client
     Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
@@ -17,7 +13,7 @@ class TwitterFeed < Feed
     TwitterFeed.client.user_timeline(self.uid.to_i)
   end
 
-  def save_posts #consider making validations
+  def save_posts #consider making validations and a find or create by method to eliminate feed duplication bug
     client.user_timeline(self.uid.to_i).each do |tweet|
       Post.create!(feed_id: id, content: tweet.text, author: tweet.user.name, post_date: tweet.created_at, content_type: "tweet" )
     end  
