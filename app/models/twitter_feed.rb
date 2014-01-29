2f865834c15c7fa658195ee6183df210a43c7486
Melinda Mizuta <mkmizuta@gmail.com>
2014-01-29 15:23:32 -0800

    updated twitter based on TA's input

--- app/models/twitter_feed.rb
+++ app/models/twitter_feed.rb
@@ -15,7 +15,13 @@ class TwitterFeed < Feed
 
   def save_posts #consider making validations and a find or create by method to eliminate feed duplication bug
     client.user_timeline(self.uid.to_i).each do |tweet|
-      Post.create!(feed_id: id, content: tweet.text, author: tweet.user.name, post_date: tweet.created_at, content_type: "tweet" )
+      Post.create!(
+        feed_id: id, 
+        content: tweet.text, 
+        author: tweet.user.name, 
+        post_date: tweet.created_at, 
+        content_type: "tweet" 
+        )
     end  
   end
 end
